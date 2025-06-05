91f84582c86920bb6cb1354c7d8905a852b884ee
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2025-06-05 14:00:13 +0530

    Add display and print functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::fmt;
 use sha2::{Sha256, Digest};
 
 #[derive(Debug)]
@@ -11,6 +12,16 @@ enum Direction {
 struct Node<'a> {
     hash: &'a [u8;32],
     direction: Direction
+}
+
+impl<'a> fmt::Display for Node<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "direction: {:?}, hash: {} ", self.direction, to_hex(self.hash))
+    }
+}
+
+fn to_hex(hash: &[u8; 32]) -> String {
+    hash.iter().map(|b| format!("{:02x}", b)).collect()
 }
 
 fn hash_pair(left: &[u8;32], right: &[u8;32]) -> [u8;32] {
@@ -104,19 +115,37 @@ fn generate_merkle_tree(hashes: &Vec::<i32> ) -> Vec<Vec<[u8;32]>> {
     return tree;
 }
 
+fn print_level(branch: &Vec<[u8;32]>) {
+    for hash in branch {
+        println!(" {}",to_hex(hash));
+    }
+}
+
+fn print_tree(tree: &Vec<Vec<[u8;32]>>) {
+    for (level, branch) in tree.iter().enumerate() {
+        println!("level {}", level);
+        print_level(&branch);
+    }
+}
+
+fn print_proof(tree: &Vec<Node>) {
+    for node in tree {
+        println!("{}", node);
+    }
+}
+
 fn main() {
-    let content = fs::read_to_string("data.txt").unwrap();
+    let content = fs::read_to_string("data.txt").expect("Failed to read data.txt");
     let leaf_nodes:Vec<i32> = content.split(' ').map(|s| s.trim().parse::<i32>().expect("Invalid number")).collect();
-    println!("{:?}",leaf_nodes);
 
     let tree:Vec<Vec<[u8;32]>> = generate_merkle_tree(&leaf_nodes);
-    println!("{:?}",tree);
+    print_tree(&tree);
 
     let mut hasher = Sha256::new();
     hasher.update(&leaf_nodes[0].to_string());
     let target_hash = hasher.finalize().into();
 
     let merkle_proof: Vec<Node> = generate_merkle_proof(&target_hash, &tree);
-    println!("{:?}",merkle_proof);
+    print_proof(&merkle_proof);
 
 }
